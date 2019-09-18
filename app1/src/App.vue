6ef03cbc9a86d1ba345725f616ba6074718043f0
niuyueyang <niuyueyang@gaosiedu.com>
2019-09-18 15:46:35 +0800

    'vue多系统架构'

---
+++ app1/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <span id="app1">
+    App1 is working!
+    <router-view></router-view>
+  </span>
+</template>
+
+<style>
+#app1 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
